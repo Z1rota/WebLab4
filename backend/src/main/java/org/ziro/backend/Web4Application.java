ecccc249f35dfd1c39c36c5737ff5741a6a68137
Ziro <Global_counter@mail.ru>
2025-11-13 13:25:39 +0300

    Refactor standard java class

--- backend/src/main/java/org/ziro/backend/Web4Application.java
+++ backend/src/main/java/org/ziro/backend/Web4Application.java
@@ -4,10 +4,7 @@ import jakarta.ws.rs.ApplicationPath;
 import jakarta.ws.rs.core.Application;
 
 @ApplicationPath("/api")
-public class HelloApplication extends Application {
+public class Web4Application extends Application {
 
-    public HelloApplication() {
-        super();
-    }
 
 }
