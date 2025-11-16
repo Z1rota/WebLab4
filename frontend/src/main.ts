7b796745fd1fa313d87c6e1d4665a0fe1879a026
Ziro <Global_counter@mail.ru>
2025-11-16 19:15:36 +0300

    update(frontend) part2

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,11 +2,15 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { provideHttpClient } from '@angular/common/http'; 
 import { importProvidersFrom } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { LoginComponent } from './app/auth/login.component';
+import { LoginComponent } from './app/auth/login/login.component';
+import { RegisterComponent } from './app/auth/register/register.component';
+import { provideRouter } from '@angular/router';
+import { routes } from "./app/app.routes"
+import { AppComponent } from './app/app.component';
 
-bootstrapApplication(LoginComponent, {
+bootstrapApplication(AppComponent, {
   providers: [
-   
+    provideRouter(routes),
     provideHttpClient(), 
     importProvidersFrom(FormsModule) 
   ]
